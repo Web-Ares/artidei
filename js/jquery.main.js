f59a1c58cab20211cf720bbeeacddd35ed1a9009
epetrunin <petruninevgeniy@gmail.com>
2015-10-14 11:05:44 +0300

    stared contacts page

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -19,4 +19,31 @@ $(function(){
         autoplayDisableOnInteraction: false
     });
 
+
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [55.849974, 37.670016],
+                zoom: 9
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'Собственный значок метки',
+                balloonContent: 'Это красивая метка'
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: 'img/myIcon.png',
+                // Размеры метки.
+                iconImageSize: [113, 108],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-3, -42]
+            });
+
+        myMap.geoObjects.add(myPlacemark);
+    });
+
 } );
