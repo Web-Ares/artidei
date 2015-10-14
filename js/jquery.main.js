e182b5af9a16575786f74488c54808ffd461ce6e
cybers8502 <maks-ua@list.ru>
2015-10-14 11:02:57 +0300

    portfolio

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,26 +1,29 @@
 $(function(){
 
-    $('.promo').each(function(){
-        var swiper = new Swiper('.promo-swiper', {
-            pagination: '.promo-pagination',
-            nextButton: '.promo-next',
-            prevButton: '.promo-prev',
-            paginationClickable: true,
-            loop:true,
-            autoplay: 5000,
-            autoplayDisableOnInteraction: false
+    if ($('.promo').length){
+        $('.promo').each(function(){
+            var swiper = new Swiper('.promo-swiper', {
+                pagination: '.promo-pagination',
+                nextButton: '.promo-next',
+                prevButton: '.promo-prev',
+                paginationClickable: true,
+                loop:true,
+                autoplay: 5000,
+                autoplayDisableOnInteraction: false
+            });
         });
-    });
-
-    $('.reviews').each(function(){
-        var reviews = new Swiper('.reviews-swiper', {
-            pagination: '.reviews-pagination',
-            nextButton: '.reviews-next',
-            prevButton: '.reviews-prev',
-            paginationClickable: true,
-            loop:true,
-            autoplayDisableOnInteraction: false
+    }
+    if ($('.reviews').length) {
+        $('.reviews').each(function () {
+            var reviews = new Swiper('.reviews-swiper', {
+                pagination: '.reviews-pagination',
+                nextButton: '.reviews-next',
+                prevButton: '.reviews-prev',
+                paginationClickable: true,
+                loop: true,
+                autoplayDisableOnInteraction: false
+            });
         });
-    });
+    }
 
 } );
